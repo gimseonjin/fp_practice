fdfd380ef97ffc1615b9b87cd151f36c47f65404
gimseonjin616 <66009926+gimseonjin@users.noreply.github.com>
2022-11-17 16:22:01 +0900

    odd done

--- ch4.js
+++ ch4.js
@@ -11,3 +11,26 @@ function *gen() {
 let iter = gen();
 
 for(const i of iter) console.log(i)
+
+
+function *infinity(i = 0){
+    while(true) yield i++
+}
+
+
+function *limit(l, iter){
+    for(const i of iter){
+        yield i;
+        if (i == l) return;
+    }
+
+}
+
+
+function *odds(num) {
+    for(const i of limit(num, infinity(1))){
+        if (i % 2) yield i;
+    }
+}
+
+for(const o of odds(99)) console.log(o)
